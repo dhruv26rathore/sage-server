c1bc9d4b2b19bd22602ec955637b3f3fea302e6a
dhruv26rathore <64855805+dhruv26rathore@users.noreply.github.com>
2021-12-30 17:37:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,11 +36,7 @@ const corsOpts = {
       'Content-Type',
     ],
   };
-  app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-  });
+
   
   app.use(cors(corsOpts));
 // app.use(cors())
